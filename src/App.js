823cdd2973d865ec6c75d57919814785e234ed6f
eduardosmoovin <eduardo.silva@moovin.com.br>
2021-09-17 23:49:09 -0300

    Cria estrutura do projeto

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import { Home } from "./pages/Home"
+
+import './styles/global.scss'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function Home() {
+  return (
+    <main className="content">
+      Olá, mundo!
+    </main>
+  )
+}
