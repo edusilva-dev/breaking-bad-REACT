e894551205f94da9b990956e9bf373d8b34c3003
eduardosmoovin <eduardo.silva@moovin.com.br>
2021-09-18 21:12:31 -0300

    adiciona header e footer na estrutura

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
+import { Header } from './components/Header'
+import { Footer } from './components/Footer'
 import { Home } from "./pages/Home"
 
 import './styles/global.scss'
@@ -7,9 +9,11 @@ import './styles/global.scss'
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route exact path="/" component={Home} />
       </Switch>
+      <Footer />
     </BrowserRouter>
   )
 }
